6ec6ddd36e80eaf8b33b01dd9da24130b10e0c93
richardesp <71340907+richardesp@users.noreply.github.com>
2021-09-22 19:19:25 +0200

    Creación inicial del proyecto

---
+++ practica1/src/ejercicio2/Main.java
@@ -0,0 +1,41 @@
+package ejercicio2;
+
+/**
+ * Clase pública del programa main()
+ * @author Ricardo Espantaleón Pérez
+ * @version 1.0
+ */
+public class Main {
+
+	/**
+	 * Función Main del programa
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+	
+	/**
+	 * Para crear una clase de patrón Singleton, tenemos que crear
+	 * un constructor privado, al cual solo podemos llamarlo desde otro
+	 * método de la misma clase, por tanto este método debe ser público y
+	 * estático, ya que solamente podremos referenciar una única instancia,
+	 * y la instancia debe ser privada estática y nula 
+	 */
+
+	
+	/**
+	 * Para generar un ejecutable:
+	 * Empaqueto el fichero main:
+	 * 1. Ejecuto el código
+	 * 2. Clicko en la clase MAIN
+	 * 3. Le doy a exportar
+	 * 4. Dentro de la categoria de java: Runnable jar file
+	 * 5. Le paso la configuración de lanzamiento (argumentos personalizados)
+	 * 6. Le doy a finalizar
+	 * 
+	 * Puedo ejecutar el fichero .jar haciendo: java -jar <program>.jar
+	 * 
+	 */
+}
