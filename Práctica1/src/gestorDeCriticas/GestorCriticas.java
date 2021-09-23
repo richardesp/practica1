965f1cc1dce0ca1058d23f6737096367d6211d43
richardesp <71340907+richardesp@users.noreply.github.com>
2021-09-23 20:23:31 +0200

    Creación funciones de gestión de usuarios

--- Práctica1/src/gestorDeCriticas/GestorCriticas.java
+++ Práctica1/src/gestorDeCriticas/GestorCriticas.java
@@ -4,90 +4,172 @@
 package gestorDeCriticas;
 
 import java.util.ArrayList;
-import java.util.Properties;
 
 /**
  * Singleton !!!
  * 
- * @author ricardo
+ * @author Ricardo Espantaleón Pérez
+ * @author Nicolás López Delgado
  *
  */
 public class GestorCriticas {
 
 	// Variables
+
+	// Instancia del patrón de diseño singleton
 	private static GestorCriticas instance = null;
+	// Lista de usuarios
 	private ArrayList<Espectador> usuarios;
+	// Lista de críticas
 	private ArrayList<Critica> criticas;
 
 	// Métodos
+
+	/**
+	 * Función que devuelve la instancia de la clase
+	 * 
+	 * @return Devuelve la instancia de la clase
+	 * @author Ricardo Espantaleón Pérez
+	 */
 	public static GestorCriticas getInstance() {
 		if (instance == null)
 			instance = new GestorCriticas();
 		return instance;
 	}
-	
+
 	/**
-	 * Registra un usuario en el sistema (comprueba que no exista ese nick)
-	 * @author
-	 * @param
-	 * @return
+	 * Constructor sin parámetros
+	 * 
+	 * @author Ricardo Espantaleón Pérez
 	 */
-	public boolean crearUsuario(Espectador espectador) {
-		return true;
+	private GestorCriticas() {
+		this.usuarios = new ArrayList<Espectador>();
+		this.criticas = new ArrayList<Critica>();
 	}
-	
+
 	/**
-	 * Función que elimina un usuario (retorna falso si no existe y true en caso contrario)
+	 * Función que comprueba que exista un usuario en el sistema
+	 * 
+	 * @param nick Nickname del usuario
+	 * @return Verdadero si existe el usuario y falso si no existe
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public boolean existeNick(String nick) {
+		if (listaUsuariosVacia())
+			return false;
+		for (int index = 0; index < usuarios.size(); ++index) {
+			if (usuarios.get(index).getNick() == nick)
+				return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * 
+	 * @return Devuelve verdadero si la lista de usuarios está vacía
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public boolean listaUsuariosVacia() {
+		return usuarios.size() == 0;
+	}
+
+	/**
+	 * 
+	 * @return Devuelve verdadero si la lista de críticas está vacía
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public boolean listaCriticasVacia() {
+		return criticas.size() == 0;
+	}
+
+	/**
+	 * Función que devuelve un usuario si existe en el sistema
+	 * 
+	 * @exception Lanza una excepción en caso de que no exista el usuario en el
+	 *                  sistema
+	 * 
+	 * @param nick
+	 * @return Devuelve un usuario existente en el sistema
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public Espectador getUsuario(String nick) throws Exception {
+		if (!existeNick(nick))
+			throw new Exception("Usuario no existente en el sistema");
+
+		Espectador usuario = new Espectador(null, null, null);
+		for (int index = 0; index < usuarios.size(); ++index) {
+			if (usuarios.get(index).getNick() == nick) {
+				usuario = usuarios.get(index);
+				break;
+			}
+		}
+
+		return usuario;
+
+	}
+
+	/**
+	 * Registra un usuario en el sistema, siempre y cuando no exista previamente
+	 * 
+	 * @param espectador
+	 * @return Devuelve true si el usuario ha podido crearse, en caso de que ya
+	 *         exista con dicho nick devolverá false
+	 * @exception Lanza una excepción en caso de que ya exista el usuario en el
+	 *                  sistema
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public void crearUsuario(Espectador espectador) throws Exception {
+		if (existeNick(espectador.getNick()))
+			throw new Exception("Usuario ya registrado");
+		usuarios.add(espectador);
+
+	}
+
+	/**
+	 * Función que elimina un usuario (retorna falso si no existe y true en caso
+	 * contrario)
+	 * 
+	 * @exception Lanza una excepción en caso de que no exista el usuario en el
+	 *                  sistema
 	 * @param nick
 	 * @return
+	 * @author Ricardo Espantaleón Pérez
 	 */
-	public boolean eliminarUsuario(String nick) {
-		return true;
+	public void eliminarUsuario(String nick) throws Exception {
+		if (!existeNick(nick))
+			throw new Exception("Usuario no existente en el sistema");
+		Espectador espectador = getUsuario(nick);
+		usuarios.remove(espectador);
 	}
-	
-	/**
-	 * Función que consulta un usuario y si no existe lanza una excepción
-	 * @param nick
-	 * @return
-	 */
-	public Espectador consultarUsuario(String nick) /*throw Exception*/ {
-		return null;
-	}
-	
+
 	/**
 	 * LLamar a set del espectador
+	 * 
 	 * @param nombreApellidos
 	 * @return
 	 */
 	public boolean actualizarNombreApellidosUsuario(String nick, String nombreApellidos) {
 		return true;
 	}
-	
+
 	/**
 	 * LLamar a set del espectador
+	 * 
 	 * @param email
 	 * @return
 	 */
 	public boolean actualizarEmailUsuario(String nick, String email) {
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @param nick
 	 * @param critica
 	 */
 	public void crearCritica(String nick, Critica critica) {
-		
-	}
-	
-	/**
-	 * 
-	 * @param nick
-	 * @return
-	 */
-	private boolean existeNick(String nick) {
-		return true;
+
 	}
 
 	/**
@@ -96,9 +178,9 @@ public class GestorCriticas {
 	 * @param critica
 	 */
 	public void borrarCritica(String nick, String titulo) {
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @return
@@ -106,17 +188,17 @@ public class GestorCriticas {
 	public ArrayList<Critica> consultarCriticas() {
 		return null;
 	}
-	
+
 	/**
 	 * 
 	 * @param nick
 	 * @param titulo
 	 * @return
 	 */
-	public boolean votarCritica(String nick, string titulo) {
-		
+	public boolean votarCritica(String nick, String titulo) {
+		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @param nick
@@ -125,7 +207,19 @@ public class GestorCriticas {
 	public ArrayList<Critica> consultarCriticasUsuario(String nick) {
 		return null;
 	}
-	
-	
-	
+
+	/**
+	 * Función que devuelve la lista de todos los usuarios registrados en el sistema
+	 * 
+	 * @exception Lanza una excepción en caso de que la lista de usuarios en el
+	 *                  sistema esté vacía
+	 * @return Devuelve la lista de usuarios del sistema
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	public ArrayList<Espectador> getUsuarios() throws Exception {
+		if (listaUsuariosVacia())
+			throw new Exception("Lista de usuarios vacía");
+		return usuarios;
+	}
+
 }
