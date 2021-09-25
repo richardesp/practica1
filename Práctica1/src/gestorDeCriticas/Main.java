79f3729ef8dad7e4a6e244288885feb7b9912382
richardesp <71340907+richardesp@users.noreply.github.com>
2021-09-25 10:29:52 +0200

    Subida de un main de pruebas

--- Práctica1/src/gestorDeCriticas/Main.java
+++ Práctica1/src/gestorDeCriticas/Main.java
@@ -1,10 +1,12 @@
 package gestorDeCriticas;
 
 /**
- * @author ricardo
- *	Espectador, Espectaculo, gestorDeCriticas (singleton)
- *	
+ * @author ricardo Espectador, Espectaculo, gestorDeCriticas (singleton)
+ * 
  */
+
+import java.util.ArrayList;
+
 public class Main {
 
 	/**
@@ -12,6 +14,34 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("HOLA");
+		GestorCriticas gestorCriticas = GestorCriticas.getInstance();
+		Espectador e1 = new Espectador("Ricardo", null, null);
+		Espectador e2 = new Espectador("Nico", null, null);
+		Espectador e3 = new Espectador("Olga", null, null);
+		ArrayList<Espectador> usuarios = new ArrayList<Espectador>();
+
+		try {
+			Critica c = new Critica("Hola", (float) 5.0, "hola", e1);
+			c.setPuntuacion((float) 5.6);
+			gestorCriticas.crearUsuario(e1);
+			/*
+			 * gestorCriticas.crearUsuario(e1); gestorCriticas.crearUsuario(e2);
+			 * gestorCriticas.crearUsuario(e3); if(!gestorCriticas.listaUsuariosVacia())
+			 */
+			usuarios = gestorCriticas.getUsuarios();
+
+		} catch (Exception error) {
+			error.printStackTrace();
+		}
+
+		System.out.println("Aqui sigo, me sigo imprimiendo pese al catch");
+
+		/*
+		 * for(int i = 0; i < usuarios.size(); ++i)
+		 * System.out.println(usuarios.get(i).getNick());
+		 */
 
 	}
 
