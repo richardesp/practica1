427bd1cb4e06136c510449792b8d57c953baf8da
nicklodel <71691711+nicklodel@users.noreply.github.com>
2021-09-25 13:14:47 +0200

    Fully operational GestorCriticas
    He añadido realizado las funciones que quedaban por completar: actualizarNombreApellidosUsuario, actualizarEmailUsuario, crearCritica, borrarCritica, getCriticas, votarCritica y getCriticaUsuario

--- Práctica1/src/gestorDeCriticas/GestorCriticas.java
+++ Práctica1/src/gestorDeCriticas/GestorCriticas.java
@@ -144,70 +144,135 @@ public class GestorCriticas {
 		Espectador espectador = getUsuario(nick);
 		usuarios.remove(espectador);
 	}
-
+	
 	/**
-	 * LLamar a set del espectador
+	 * La función cambia el nombre y apellidos a un usuario
 	 * 
-	 * @param nombreApellidos
+	 * @param nombreApellidos el nombre nuevo que se quiere cambiar
+	 * @param nick            identificador del usuario
 	 * @return
+	 * @exception Lanza una excepción en caso de que no exista el usuario en el
+	 *                  sistema
+	 * @author Nicolás López Delgado
 	 */
-	public boolean actualizarNombreApellidosUsuario(String nick, String nombreApellidos) {
-		return true;
+	public void actualizarNombreApellidosUsuario(String nick, String nombreApellidos) throws Exception {
+		if (!existeNick(nick))
+			throw new Exception("Usuario no existente en el sistema");
+		int indiceUsuario = -1;
+		for (int i = 0; i < usuarios.size(); i++) {
+			if (nick == usuarios.get(i).getNick()) {
+				indiceUsuario = i;
+			}
+		}
+		usuarios.get(indiceUsuario).setNombreApellidos(nombreApellidos);
+
 	}
 
 	/**
-	 * LLamar a set del espectador
+	 * La funcion actualiza el email de un usuario
 	 * 
-	 * @param email
+	 * @param email (El email que se quiere cambiar)
+	 * @param nick  identificador de usuario
+	 * @return true si se ha podido crear y false si no se consiguió
+	 * @exception Lanza una excepción en caso de que no exista el usuario en el
+	 *                  sistema
+	 * @author Nicolás López
+	 */
+	public void actualizarEmailUsuario(String nick, String email) throws Exception {
+		if (!existeNick(nick))
+			throw new Exception("Usuario no existente en el sistema");
+		int indiceUsuario = -1;
+		for (int i = 0; i < usuarios.size(); i++) {
+			if (nick == usuarios.get(i).getNick()) {
+				indiceUsuario = i;
+			}
+		}
+		usuarios.get(indiceUsuario).setEmail(email);
+	}
+	/**
+	 * La función añade una crítica nueva al vector
+	 * 
+	 * @param critica la crítica ya inicializada
 	 * @return
+	 * @author Nicolás López
+	 * 
 	 */
-	public boolean actualizarEmailUsuario(String nick, String email) {
-		return true;
+	public void crearCritica(Critica critica) throws Exception {
+		if (!existeNick(critica.getNickAutor()))
+			throw new Exception("Usuario no existente en el sistema");
+		criticas.add(critica);
 	}
 
 	/**
+	 * La función borra una critica del vector criticas
 	 * 
-	 * @param nick
-	 * @param critica
-	 */
-	public void crearCritica(String nick, Critica critica) {
-
-	}
-
-	/**
-	 * 
-	 * @param nick
-	 * @param critica
+	 * @param espectador el espectador al que pertenece la crítica
+	 * @param titulo     el titulo de la crítica
+	 * @return
+	 * @author Nicolás López
 	 */
 	public void borrarCritica(String nick, String titulo) {
-
+		int indiceCritica = -1;
+		for (int i = 0; i < criticas.size(); i++) {
+			if (nick == criticas.get(i).getNickAutor() && titulo == criticas.get(i).getTitulo()) {
+				indiceCritica = i;
+			}
+		}
+		criticas.remove(indiceCritica);
 	}
 
 	/**
+	 * Devuelve todas las críticas del gestor
+	 *
 	 * 
-	 * @return
+	 * @author Nicolás López
+	 * @return un vector con las críticas
 	 */
-	public ArrayList<Critica> consultarCriticas() {
-		return null;
+	public ArrayList<Critica> getCriticas() {
+		return criticas;
 	}
 
 	/**
+	 * Vota una crítica de otro autor
 	 * 
-	 * @param nick
-	 * @param titulo
+	 * @param nick   para identificar el usuario
+	 * @param titulo para identificar la crítica en concreto
+	 * @param valoracion valor de la nota
 	 * @return
+	 * @author Nicolás López
+	 * 
 	 */
-	public boolean votarCritica(String nick, String titulo) {
-		return true;
+	public void votarCritica(String nick, String titulo, float valoracion) throws Exception {
+		if (!existeNick(nick))
+			throw new Exception("Usuario no existente en el sistema");
+		if (valoracion < 0 || valoracion > 10) {
+			throw new Exception("Valoraciones deben ser un flotante entre 0 y 10");
+		}
+		int indiceCritica = -1;
+		for (int i = 0; i < criticas.size(); i++) {
+			if (nick == criticas.get(i).getNickAutor() && titulo == criticas.get(i).getTitulo()) {
+				indiceCritica = i;
+			}
+		}
+		criticas.get(indiceCritica).agregarValoracion(valoracion);
 	}
 
 	/**
+	 * Función que devuelve las críticas de un usuario específico
 	 * 
-	 * @param nick
-	 * @return
+	 * @param nick el identificador de usuario
+	 * @return un vector con las críticas del usuario
+	 * @author Nicolás López
 	 */
-	public ArrayList<Critica> consultarCriticasUsuario(String nick) {
-		return null;
+	public ArrayList<Critica> getCriticasUsuario(String nick) {
+		ArrayList<Critica> criticasUsuario = new ArrayList<Critica>();
+		for (int i = 0; i < criticas.size(); i++) {
+			if (nick == criticas.get(i).getNickAutor()) {
+				criticasUsuario.add(criticas.get(i));
+			}
+
+		}
+		return criticasUsuario;
 	}
 
 	/**
