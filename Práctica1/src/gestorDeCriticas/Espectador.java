c3aea10de90232217f8dd666a5e0b67fd7eedc5a
richardesp <71340907+richardesp@users.noreply.github.com>
2021-09-27 13:36:47 +0200

    Uso de REGEX para la comprobación del email

--- Práctica1/src/gestorDeCriticas/Espectador.java
+++ Práctica1/src/gestorDeCriticas/Espectador.java
@@ -3,6 +3,9 @@
  */
 package gestorDeCriticas;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * La clase Espectador con atributos nombre y apellidos, nick , y email
  * 
@@ -16,6 +19,8 @@ public class Espectador {
 	private String nombreApellidos;
 	private String nick;
 	private String email;
+	private Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+			Pattern.CASE_INSENSITIVE);
 
 	// Metodos
 
@@ -29,10 +34,21 @@ public class Espectador {
 	 * @author Enrique Estévez Mayoral
 	 * @author Ricardo Espantaleón Pérez
 	 */
-	Espectador(String nick, String nombreApellidos, String email) {
+	Espectador(String nick, String nombreApellidos, String email) throws Exception {
+		if (!validarEmail(email))
+			throw new Exception("El formato del email es incorrecto");
 		this.nick = nick;
 		this.nombreApellidos = nombreApellidos;
 		this.email = email;
+	}
+
+	Espectador(Espectador e) throws Exception {
+		if (!validarEmail(e.getEmail()))
+			throw new Exception("El formato del email es incorrecto");
+
+		this.nick = e.getNick();
+		this.nombreApellidos = e.getNombreApellidos();
+		this.email = e.getEmail();
 	}
 
 	/**
@@ -101,5 +117,19 @@ public class Espectador {
 	 */
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	/**
+	 * Función para comprobar si el email introducido cumple la REGEX
+	 * 
+	 * @param email parámetro a comprobar si cumple la REGEX
+	 * @return Verdadero si cumple el formato y falso en caso contrario
+	 * 
+	 * @author Ricardo Espantaleón Pérez
+	 */
+	private boolean validarEmail(String email) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+
+		return matcher.find();
 	}
 }
