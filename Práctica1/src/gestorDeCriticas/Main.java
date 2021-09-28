9e189b5e093cea59687098492395e43d2c3db657
nicklodel <71691711+nicklodel@users.noreply.github.com>
2021-09-28 18:48:52 +0200

    Arreglado case 5 del menú de usuario y fallos

--- Práctica1/src/gestorDeCriticas/Main.java
+++ Práctica1/src/gestorDeCriticas/Main.java
@@ -145,6 +145,105 @@ public class Main {
 			case 1:
 				limpiarPantalla();
 
+				System.out.print("Introduzca su nombre de usuario: ");
+				nickName = input.nextLine();
+
+				if (gestorCriticas.existeNick(nickName)) {
+					usuarioLogeado = nickName;
+
+					menuUsuarioLogeado();
+
+				} else {
+					System.err.print("Error, el nick introducido no se encuentra en el sistema\n\n");
+					systemPause();
+
+					break;
+				}
+
+				break;
+
+			case 2:
+				limpiarPantalla();
+
+				System.out.print("Introduzca su nombre y apellido: ");
+				nombreApellidos = input.nextLine();
+				System.out.print("\n");
+
+				System.out.print("Introduzca su nickname: ");
+				nickName = input.nextLine();
+				System.out.print("\n");
+
+				System.out.print("Introduzca su email: ");
+				email = input.nextLine();
+				System.out.print("\n");
+
+				Espectador e;
+
+				// Compruebo que se crea correctamente el usuario
+				try {
+					e = new Espectador(nickName, nombreApellidos, email);
+
+				} catch (Exception error) {
+					System.err.print("Error, formato de email incorrecto\n\n");
+					systemPause();
+
+					// Me salgo del case dado que ha habido un error
+					break;
+
+				}
+
+				// Compruebo que el usuario se agrega correctamente
+				try {
+					gestorCriticas.crearUsuario(e);
+
+				} catch (Exception error) {
+					System.err.print("Error, el usuario ya ha sido registrado\n\n");
+					systemPause();
+
+					// Me salgo del case dado que ha habido un error
+					break;
+				}
+
+				System.out.print("Usuario creado correctamente.\n");
+				System.out.format("Bienvenido %s (%s)\n\n", e.getNombreApellidos(), e.getNick());
+
+				systemPause();
+
+				break;
+
+			case 3:
+				System.exit(0);
+
+			}
+
+		}
+	}
+
+	static private void menuUsuarioLogeado() {
+		int ans = 0;
+
+		while (true) {
+			limpiarPantalla();
+
+			System.out.format("Bienvenido: %s\n\n", usuarioLogeado);
+			System.out.print("[1] Crear una crítica\n\n");
+			System.out.print("[2] Consultar todas las críticas disponibles\n\n");
+			System.out.print("[3] Borrar una crítica\n\n");
+			System.out.print("[4] Votar la utilidad de una crítica\n\n");
+			System.out.print("[5] Buscar la crítica de un usuario\n\n");
+			System.out.print("[6] Cerrar sesión\n\n");
+			System.out.print("[7] Cerrar programa\n\n");
+
+			System.out.print("Introduzca una opción: ");
+			// Se guarda en el buffer un \n al ser un int
+			ans = input.nextInt();
+			input.nextLine();
+			System.out.print("\n");
+
+			switch (ans) {
+			case 1:
+				limpiarPantalla();
+
 				String titulo, comentario;
 				float puntuacion;
 
@@ -220,6 +319,16 @@ public class Main {
 					System.out.println("Crítica[" + (i + 1) + "]\n" + "Título: " + consulta.get(i).getTitulo()
 							+ "\nPuntuación: " + consulta.get(i).getPuntuacion() + "\nComentario: "
 							+ consulta.get(i).getComentario() + "\nAutor: " + consulta.get(i).getNickAutor() + "\n");
+					if (!consulta.get(i).getValoracion().isEmpty()) {
+						float media=0;
+						try {
+							media = consulta.get(i).hacerMediaValoracion();
+						} catch (Exception e) {
+							System.err.print("No hay valoraciones\n");
+						}
+						
+						System.out.println("Valoracion:" + media + "\n");
+					}
 				}
 
 				systemPause();
@@ -316,24 +425,43 @@ public class Main {
 					break;
 				}
 				
-				System.out.print("La crítica introducida ha sido eliminada correctamente\n\n");
+				System.out.print("La crítica introducida ha sido votada correctamente\n");
 				
 				systemPause();
 				break;
 
 			case 5:
-				/*
-				 * String nickABuscar;
-				 * System.out.println("Introduzca el título de la obra a valorar\n");
-				 * nickABuscar = input.nextLine(); if (!gestorCriticas.existeNick(nickABuscar))
-				 * { throw new Exception("El usuario no existe en el sistema\n"); }
-				 * ArrayList<Critica> consultaUsuario = new ArrayList<Critica>(); consulta =
-				 * gestorCriticas.getCriticasUsuario(nickABuscar); for (int i = 0; i <
-				 * consultaUsuario.size(); i++) { System.out.println("Título: " +
-				 * consultaUsuario.get(i).getTitulo() + "\nPuntuación: " +
-				 * consultaUsuario.get(i).getPuntuacion() + "\nComentario:" +
-				 * consulta.get(i).getComentario()); }
-				 */
+				limpiarPantalla();
+				String nickABuscar;
+				System.out.println("Introduzca el nick a buscar\n");
+				nickABuscar = input.nextLine();
+				ArrayList<Critica> consultaUsuario = new ArrayList<Critica>();
+				
+				try {
+					consultaUsuario = gestorCriticas.getCriticasUsuario(nickABuscar);
+				} catch(Exception e) {
+					System.err.print("El usuario no existe\n");
+					
+					systemPause();
+					break;
+				}
+				
+				for (int i = 0; i < consultaUsuario.size(); i++) {
+					System.out.println("Título: " + consultaUsuario.get(i).getTitulo() + "\nPuntuación: "
+							+ consultaUsuario.get(i).getPuntuacion() + "\nComentario:"
+							+ consultaUsuario.get(i).getComentario() + "\n");
+					if (!consultaUsuario.get(i).getValoracion().isEmpty()) {
+						float media=0;
+						try {
+							media = consultaUsuario.get(i).hacerMediaValoracion();
+						} catch (Exception e) {
+							System.err.print("No hay valoraciones\n");
+						}
+						
+						System.out.println("Valoracion:" + media + "\n");
+					}
+				}
+				systemPause();
 				break;
 
 			case 6:
