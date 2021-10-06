c997cc00d38371375660eaa3a80e6cf9b60b5132
EnriqueEstevez <71635097+EnriqueEstevez@users.noreply.github.com>
2021-10-06 09:13:12 +0200

    Update función fechaMayorActual

--- Ejercicio 2/src/gestorDeEspectaculos/EspectaculoPuntual.java
+++ Ejercicio 2/src/gestorDeEspectaculos/EspectaculoPuntual.java
@@ -125,7 +125,7 @@ public class EspectaculoPuntual extends Espectaculo {
 
 		Date fechaActual = new Date();
 
-		if (fechaActual.compareTo(fechaEspectaculo) > 0)
+		if (fechaEspectaculo.compareTo(fechaActual) > 0)
 			return true;
 
 		return false;
