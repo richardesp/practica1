e8c3b9575e2c576e76368be5f973a05dcea960dc
EnriqueEstevez <71635097+EnriqueEstevez@users.noreply.github.com>
2021-10-09 12:55:44 +0200

    Update EspectaculoPuntual.java

--- Ejercicio 2 v3.4 snapshot minecraft cliff and caves/src/gestorDeEspectaculos/EspectaculoPuntual.java
+++ Ejercicio 2 v3.4 snapshot minecraft cliff and caves/src/gestorDeEspectaculos/EspectaculoPuntual.java
@@ -1,15 +1,10 @@
 package gestorDeEspectaculos;
 
-
 import java.util.ArrayList;
 
 public class EspectaculoPuntual extends Espectaculo {
 
 	// Variables
-	String titulo;
-	String categorias;
-	String descripcion;
-	ArrayList<Sesion> sesiones;
 
 	// Métodos
 
@@ -19,20 +14,16 @@ public class EspectaculoPuntual extends Espectaculo {
 	 * @param titulo      Cadena que contiene el título del espectáculo
 	 * @param categorias  Cadena que contiene las categorias del espectáculo
 	 * @param descripcion Cadena que contiene la descrpcion del espectáculo
-	 * @param sesiones    Clase de tipo Sesion que contiene la fecha y localidades
+	 * @param sesion      Clase de tipo Sesion que contiene la fecha y localidades
 	 *                    de lasesion de un espectaculo
 	 * @author Enrique Estevez Mayoral
 	 */
-	public EspectaculoPuntual(String titulo, String categorias, String descripcion, Sesion sesion) {
-		super (titulo,categorias,descripcion);
+	public EspectaculoPuntual(String titulo, String categorias, String descripcion, ArrayList<Sesion> sesion) {
+		super(titulo, categorias, descripcion, sesion);
 		this.titulo = titulo;
 		this.categorias = categorias;
 		this.descripcion = descripcion;
-		this.sesiones = new ArrayList<Sesion>();
-		sesiones.add(sesion);
+		this.sesiones = sesion;
 	}
-	public ArrayList<Sesion> getSesiones() {
-		return sesiones;
-	}
-	
+
 }
